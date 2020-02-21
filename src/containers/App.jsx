1529c0f496d47683d3ce3fcc302160bcd9f6aed3
Diego Camino <diego.camino.r@gmail.com>
2020-02-21 00:28:15 -0500

    Conectando la información de la API

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -8,42 +8,38 @@ import Footer from '../components/Footer';
 import '../assets/styles/App.scss';
 
 const App = () => {
-  const [ videos, setVideos ] = useState([]);
-
+  const [ videos, setVideos ] = useState({ mylist: [], trends: [], originals: [] });
   useEffect(() => {
     fetch('http://localhost:3000/initialState')
       .then(response => response.json())
       .then(data => setVideos(data));
   }, []);
 
-  console.log(videos);
-
   return (
     <div className="App">
       <Header />
       <Search />
-
-      <Categories title="Mi lista">
-        <Carousel>
-          <CarouselItem />
-          <CarouselItem />
-          <CarouselItem />
-          <CarouselItem />
-        </Carousel>
-      </Categories>
+      {videos.mylist.length > 0 &&
+          <Categories title="Mi lista">
+            <Carousel>
+              <CarouselItem />
+            </Carousel>
+          </Categories>
+      }
 
       <Categories title="Tendencias">
         <Carousel>
-          <CarouselItem />
-          <CarouselItem />
+          {videos.trends.map((item)=>
+           <CarouselItem key={item.id} {...item}/>
+          )}
         </Carousel>
       </Categories>
 
       <Categories title="Originales de Plazi Video">
         <Carousel>
-          <CarouselItem />
-          <CarouselItem />
-          <CarouselItem />
+          {videos.originals.map((item)=>
+           <CarouselItem key={item.id} {...item}/>
+          )}
         </Carousel>
       </Categories>
 
