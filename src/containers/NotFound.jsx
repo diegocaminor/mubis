cf7019e29604540e69f31fba7c964aac1ed96be1
Diego Camino <diego.camino.r@gmail.com>
2020-02-23 23:06:03 -0500

    Adición container 404 not found

---
+++ src/containers/NotFound.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import '../assets/styles/components/Register.scss';
+
+const NotFound = () => (
+  <React.Fragment>
+    <h1>No encontrado</h1>
+    <h2>Regresa al home</h2>
+  </React.Fragment>
+);
+
+export default NotFound;
