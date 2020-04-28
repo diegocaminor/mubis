9eda377987cc89b6948651b8782a3e2ce4760b60
Diego Camino <diego.camino.r@gmail.com>
2020-04-28 01:57:47 -0500

    Adición redux-thunk y configuración de registro de usuario

--- src/server/server.js
+++ src/server/server.js
@@ -20,6 +20,7 @@ import passport from require("passport");
 import session from require("express-session");
 import axios from require("axios");
 
+const { config } = require("./config");
 dotenv.config();
 
 const { ENV, PORT } = process.env;
@@ -147,13 +148,21 @@ app.post("/auth/sign-up", async function (req, res, next) {
   const { body: user } = req;
 
   try {
-    await axios({
+    const userData = await axios({
       url: `${config.apiUrl}/api/auth/sign-up`,
       method: "post",
-      data: user,
+      data: {
+        'email': user.email,
+        'name': user.name,
+        'user': user.password
+      },
     });
 
-    res.status(201).json({ message: "user created" });
+    res.status(201).json({
+      name: req.body.name,
+      email: req.body.email,
+      id: userData.data.id
+    });
   } catch (error) {
     next(error);
   }
